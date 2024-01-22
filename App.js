5e21e656d224f47345f9ab6cb4066af3250001e0
Danny Dan <115761695+danycrak@users.noreply.github.com>
2024-01-22 02:01:19 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import ImageViewer from './Components/ImageViewer';
+import Button from './Components/Button';
+const PlaceholderImage = require('./assets/Image/img1.jpg')
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+      <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+ 
+});
